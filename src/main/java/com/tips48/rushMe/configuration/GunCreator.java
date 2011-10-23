97fa7cb24f6d7972cc2fe14a7d9bd7d4ca125e38
Aidan Matzko <amatzko48@gmail.com>
2011-10-23 18:36:27 -0400

    Add GunCreator, TODO

---
+++ src/main/java/com/tips48/rushMe/configuration/GunCreator.java
@@ -0,0 +1,15 @@
+package com.tips48.rushMe.configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Aidan
+ * Date: 10/23/11
+ * Time: 6:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GunCreator {
+
+    public static void main(String[] args) {
+                     // TODO gun creation GUI
+    }
+}
